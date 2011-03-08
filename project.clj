bd32e7f3510cc25f6204943fe038fdbd4cd8bd41
Kevin Downey (hiredman) <redchin@gmail.com>
2011-03-07 23:30:16 -0800

    version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject longji "1.0.0"
+(defproject longji "1.0.1-SNAPSHOT"
   :description "Check for finer grained access controls"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
